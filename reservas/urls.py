59d721fdf790aa26a405c2688a31991a3974f190
theansw3r <141658817+theansw3r@users.noreply.github.com>
2026-08-05 07:53:54 -0500

    Delete reservas directory

--- reservas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from reservas.views import CrearReservaView
-
-app_name = "reservas"
-
-urlpatterns = [
-    path("reservas/", CrearReservaView.as_view(), name="crear"),
-]
